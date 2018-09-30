b97c6750ba723f7601ae06d5e8e3cb914573f054
yuanhg <faquir.yuen@outlook.com>
2018-09-30 16:18:03 +0800

    关于GB2312和UTF-8编码的练习

--- Hello.py
+++ Hello.py
@@ -1,4 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 print('Hello,World!')
 print('The quick brown fox','jumps over','the lazy dog')
 name = input('Please enter your name: ')
 print('hello,',name)
+
+s = 'Python-中文'
+print(s)
+b = s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
