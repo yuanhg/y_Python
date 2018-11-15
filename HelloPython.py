f7bbd191e78919804748111ba8722f423e9d799a
yuanhg <faquir.yuen@outlook.com>
2018-11-15 12:07:25 +0800

    20181101

--- HelloPython.py
+++ HelloPython.py
@@ -78,3 +78,64 @@ def YHtriangles():
 #﻿﻿zip()函数接受一系列可迭代对象作为参数，将不同对象中相对应的元素打包成一个元组（tuple），
 #返回由这些元组组成的list列表，如果传入的参数的长度不等，则返回的list列表的长度和传入参数中最短对象的长度相同。
         triangles=[sum(i) for i in zip([0]+triangles,triangles+[0])]
+
+
+#求自然数中第xxx个素数
+def _odd_iter():
+    n = 1
+    while True:
+        n = n+2
+        yield n
+
+def _not_divisible(n):
+    return lambda x: x % n >0
+
+def primes():
+    yield 2
+    it = _odd_iter()   #初始序列
+    while True:
+        n = next(it)
+        yield n
+        it = filter(_not_divisible(n),it) #构造新的序列
+
+def primesNo(x):  #打印出第x个素数
+    i=0
+    pp=primes()
+    while i<x:
+        n=next(pp)
+        i+=1
+    print(n)
+
+def primesNo2(x):  #打印出第x个素数
+    i=1
+    for n in primes():
+        i+=1
+        if i>x:
+            break
+    print(n)
+
+#回数是指从左向右读和从右向左读都是一样的数，例如12321，909。
+#请利用filter()筛选出回数
+
+def o_iter():
+    i=0
+    while True:
+        i+=1
+        yield i
+
+def oio(n):
+    if n==int(str(n)[::-1]):
+        return True
+    return False
+    
+def oxxo():
+    p = o_iter()
+    pp = filter(oio,p)
+    return pp
+
+
+
+        
+
+
+        
