a4ef9b8befeb8a72bb065488e2989f7c61304c35
yuanhg <faquir.yuen@outlook.com>
2019-01-22 20:26:09 +0800

    2019.1.20

--- StepPython.py
+++ StepPython.py
@@ -6,18 +6,16 @@ with open('D:/见与不见.txt','r')    as  pif:
         if "那里" in line:
             print(line)
 
-with open('D:/Pi10000.txt','r')    as  pif:
+with open('D:/Pi1000.txt','r')    as  pif:
     for line in pif:
+        print(len(line))
         print(line)
-
-with open('D:/Pi10000.txt','rb')    as  pif:
-    for line in pif:
-        print(line)
+        
 
 def fninpi():
     ss=str(input("Pls input a Number :"))
     ts=''
-    with open('D:/Pi - Dec - Chudnovsky.txt','r')    as  pif:
+    with open('D:/Pi1000.txt','r')    as  pif:
         for line in pif:
             '''构造第一个和输入数字字符串等长的字符串'''
             for i in range(len(ss)):
@@ -27,10 +25,20 @@ def fninpi():
             num=1
             while True:
                 if ss==ts:
-                    print("在圆周率小数点第",n-len(ss)-1,"位第",num,"次出现了",line[(n-len(ss)):n])
-                    ts=ts[1:]+line[n]
+                    print("在圆周率中（含小数点）第",n-len(ss)+1,"位第",num,"次出现了",line[(n-len(ss)):n])
                     num+=1
-                else:
-                    ts=ts[1:]+line[n]
+                if n==len(line):
+                    break
+                ts=ts[1:]+line[n]
                 n+=1
-            
+            print("搜索完毕")
+
+def pipi():
+    with open('D:/Pi - Dec - Chudnovsky.txt','r') as pi1, open('D:/Pi1000.txt','r') as pi2:
+        for line1 ,line2 in zip(pi1,pi2):
+            print(len(line1),len(line2))
+            for i in range(len(line2)):
+                if line1[i]!=line2[i]:
+                    print("第",i+1,"位不同，正确的数字是",line1[i],"不是",line2[i])
+                
+                       
