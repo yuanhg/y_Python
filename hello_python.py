907e64d41d5971acb271372e174ec84c5557264e
yuanhg <faquir.yuen@outlook.com>
2019-01-31 17:50:13 +0800

    2019.1.31

---
+++ hello_python.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+'''
+The Zen of Python, by Tim Peters
+Python之禅 by Tim Peters
+
+Beautiful is better than ugly.
+优美胜于丑陋（Python 以编写优美的代码为目标）
+Explicit is better than implicit.
+明了胜于晦涩（优美的代码应当是明了的，命名规范，风格相似）
+Simple is better than complex.
+简洁胜于复杂（优美的代码应当是简洁的，不要有复杂的内部实现）
+Complex is better than complicated.
+复杂胜于凌乱（如果复杂不可避免，那代码间也不能有难懂的关系，要保持接口简洁）
+Flat is better than nested.
+扁平胜于嵌套（优美的代码应当是扁平的，不能有太多的嵌套）
+Sparse is better than dense.
+间隔胜于紧凑（优美的代码有适当的间隔，不要奢望一行代码解决问题）
+Readability counts.
+可读性很重要（优美的代码是可读的）
+Special cases aren't special enough to break the rules.
+即便假借特例的实用性之名，也不可违背这些规则（这些规则至高无上）
+Although practicality beats purity.
+当存在多种可能，不要尝试去猜测
+Errors should never pass silently.
+不要包容所有错误
+Unless explicitly silenced.
+除非你确定需要这样做（精准地捕获异常，不写 except:pass 风格的代码）
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+而是尽量找一种，最好是唯一一种明显的解决方案（如果不确定，就用穷举法）
+虽然这并不容易，因为你不是 Python 之父（这里的 Dutch 是指 Guido ）
+Now is better than never.
+做也许好过不做
+Although never is often better than *right* now.
+但不假思索就动手还不如不做（动手之前要细思量）
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+如果你无法向人描述你的方案，那肯定不是一个好方案；反之亦然（方案测评标准）
+Namespaces are one honking great idea -- let's do more of those!
+命名空间是一种绝妙的理念，我们应当多加利用（倡导与号召）
+'''
+#简单的输入输出
+print('Hello,World!')
+print('The quick brown fox',' jumps over','the lazy dog')
+
+cars = ['bmw','Audi','toyota','suBrU']
+print(cars)
+print
+'''
+yName = input('Please enter your name: ')
+print('Hello,',yNname)
+
+#编码
+s = 'Python-中文'
+print(s)
+b = s.encode('utf-8')
+print(b)
+print(b.decode('utf-8'))
+
+#字典
+yDict = {'name': 'Zara', 'age': 47}
+#字典转为字符串
+str(yDict)
+"{'name': 'Zara', 'age': 47}"
+#字典可以转为元组
+tuple(yDict)
+('name', 'age')
+#字典可以转为元组
+tuple(yDict.values())
+('Zara', 47)
+#字典转为列表
+list(yDict)
+['name', 'age']
+#字典转为列表
+list(yDict.values())
+['Zara', 47]
+
+#元组
+yTuple=(1, 2, 3, 4, 5,6,7,8)
+#元组转为字符串
+yTuple.__str__()
+'(1, 2, 3, 4, 5, 6, 7, 8)'
+#元组转为列表
+list(yTuple)
+[1, 2, 3, 4, 5, 6, 7, 8]
+#元组不可以转为字典*
+
+#列表
+yList=[1, 3, 5, 7, 9, 11, 13]
+#列表转为字符串
+str(yList)
+'[1, 3, 5, 7, 9, 11, 13]'
+#列表转为元组
+tuple(yList)
+(1, 3, 5, 7, 9, 11, 13)
+#列表不可以转为字典*
+
+#字符串
+yStr="(1,2,3)"
+#字符串转为元组
+tuple(eval(yStr))
+(1, 2, 3)
+#字符串转为列表
+list(eval(yStr))
+[1, 2, 3]
+#字符串转为字典
+yStr1="{'name':'yuan', 'age':46}"
+eval(yStr1)
+{'name': 'yuan', 'age': 46}
+
+#复杂列表
+L=[
+	['apple','google','microsoft'],
+	['java','phthon','php'],
+	['adam','bart','lisa']
+	]
+
+print(L[2][2])
+lisa
+print(L[1][-2:])
+['phthon', 'php']
+
+'''       
--- himawari8.py
+++ himawari8.py
@@ -30,24 +30,26 @@ def fill_img(img, img_save_path):
 
 
 def dl_main():
+    for i in range(48):
     # 获取当前系统时间
-    utc_today = datetime.datetime.utcnow() - datetime.timedelta(minutes=30)  # 获取GMT时间并减去30分钟
-    delat_utc_today = utc_today.strftime("%Y/%m/%d/%H%M")  # 时间格式化
+        utc_today = datetime.datetime.utcnow() - datetime.timedelta(minutes=10*i)  # 获取GMT时间并减去30分钟
+        delat_utc_today = utc_today.strftime("%Y/%m/%d/%H%M")  # 时间格式化
     # 分钟向下取整
-    delat_utc_today_list = list(delat_utc_today)
-    delat_utc_today_list[-1] = "0"
-    delat_utc_today = "".join(delat_utc_today_list)
+        delat_utc_today_list = list(delat_utc_today)
+        delat_utc_today_list[-1] = "0"
+        delat_utc_today = "".join(delat_utc_today_list)
     # 整合为链接 格式为：http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/2018/09/25/065000_0_0.png
-    img_url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/" + delat_utc_today + "00_0_0.png"
-    name = delat_utc_today.replace("/", "_") + "00_0_0.png"  # 获取图片名字
+        img_url = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/1d/550/" + delat_utc_today + "00_0_0.png"
+        name = delat_utc_today.replace("/", "_") + "00_0_0.png"  # 获取图片名字
     # 图片保存路径
-    img_save_path = "d:/wallpapers" + name
-    new_img_save_path = "d:/wallpapers/f_" + name
+        img_save_path = "d:/wallpapers/" + name
+        new_img_save_path = "d:/wallpapers/f_" + name
     # 下载图片
-    download_img(img_url, img_save_path)
+        download_img(img_url, img_save_path)
     # 合成图片
-    img = Image.open(img_save_path)
-    fill_img(img, new_img_save_path)
+        #img = Image.open(img_save_path)
+        #fill_img(img, new_img_save_path)
+        
     return new_img_save_path
 
 
